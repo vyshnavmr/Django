67bc7b98fba68f9c91bf9f53e25dc8c5d618b4df
Vyshnav <vyshnav2004sk@gmail.com>
2026-03-24 12:07:06 +0530

    created first app: 'intro' Created templates : 'homepage.html' Created first view to load homepage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from intro import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', views.home),
 ]
