a965c4ae96e0e5ffab086b6b3911a389a5e1fa65
Vyshnav <vyshnav2004sk@gmail.com>
2026-03-24 22:33:03 +0530

    added homepage and about section to home page Added a clickable button in homepage

--- mysite/intro/views.py
+++ mysite/intro/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'homepage.html')
+
+def about(request):
+    return render(request, 'about.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,4 +21,5 @@ from intro import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', views.home),
+    path('about', views.about, name='about'),
 ]
